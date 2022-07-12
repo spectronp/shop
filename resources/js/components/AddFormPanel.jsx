3f1e083630a08868ed802d4752170642db8e36de
SpecTro <natplayspectro@gmail.com>
2022-07-12 16:42:05 -0300

    Create AddFormPanel

---
+++ resources/js/components/AddFormPanel.jsx
@@ -0,0 +1,16 @@
+
+export default function AddFormPanel() {
+
+    return (
+        <form action="">
+            <input type="text" name="" id="name" />
+            <label htmlFor="name">Nome</label>
+
+            <input type="text" name="" id="about" />
+            <label htmlFor="about">Sobre</label>
+
+            <button id="register"></button>
+            <label htmlFor="register">Cadastrar</label>
+        </form>
+    )
+}
