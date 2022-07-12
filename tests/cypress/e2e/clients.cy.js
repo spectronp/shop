f276d7d8a33c3fb9beffdaf8fe4978c92b2f1bab
SpecTro <natplayspectro@gmail.com>
2022-07-12 14:19:54 -0300

    test: Fix cypress as() labels and new TODOs

--- tests/cypress/e2e/clients.cy.js
+++ tests/cypress/e2e/clients.cy.js
@@ -1,5 +1,6 @@
 describe('Clients Page', () => {
 
+    // TODO -- Replace this function by one from Testing Library
     const getByLabel = (label) => {
         return cy
             .contains('label', label)
@@ -26,13 +27,14 @@ describe('Clients Page', () => {
     cy.visit('/')
 
     // Opens and close the add form
+    // NOTE -- Assert the input is not visible before?
     cy.get('button#add-form-toggle').as('addFormToggle').click()
     getByLabel('Nome').should('be.visible')
-    cy.get('addFormToggle').click()
+    cy.get('@addFormToggle').click()
     getByLabel('Nome').should('not.be.visible')
 
     // Opens the add form and type the client name, about and click the submit button
-    cy.get('addFormToggle').click()
+    cy.get('@addFormToggle').click()
     getByLabel('Nome').type(client_data.name)
     getByLabel('Sobre').type(client_data.about)
     getByLabel('Cadastrar').click() // NOTE -- Maybe it could be an loading feedback after this click
@@ -61,7 +63,7 @@ describe('Clients Page', () => {
       })
 
     // This block tests the client card internal features
-    cy.get('addedClient').within(() => {
+    cy.get('@addedClient').within(() => {
 
         // Expand card, create history and type one line
         cy.get('.expand-toggle').as('expand').click() // NOTE -- Maybe this expand action should cover the whole client card
@@ -72,12 +74,12 @@ describe('Clients Page', () => {
         cy.contains('Salvo')
 
         // Shrink the card and assert the client data is not visible
-        cy.get('expand').click()
-        cy.get('history').should('not.be.visible')
+        cy.get('@expand').click()
+        cy.get('@history').should('not.be.visible')
 
         // Expand the card again and assert the history line is there
-        cy.get('expand').click()
-        cy.get('history').should('have.value', history_line)
+        cy.get('@expand').click()
+        cy.get('@history').should('have.value', history_line)
 
         // Click edit button,
         cy.get('.edit-button').as('edit').click()
@@ -85,7 +87,7 @@ describe('Clients Page', () => {
         getByLabel('Cancelar').click()
 
         // Click the edit button, check if the client data is present in the input, change the data and click 'Edit'
-        cy.get('.edit-button').as('edit').click()
+        cy.get('.edit-button').as('edit').click() // TODO -- Remove this as() and get the button with @edit
         getByLabel('Name')
             .should('have.value', client_data.name)
             .clear()
@@ -104,7 +106,7 @@ describe('Clients Page', () => {
         cy.contains(edited_client_data.about)
 
         // Click 'edit', then 'delete', check for 'Are u sure?' message, click 'cancel' and check if the client still there
-        cy.get('edit').click()
+        cy.get('@edit').click()
         cy.get('.delete-client').as('delete').click()
         cy.contains('Tem certeza?')
         cy.contains('A(o) cliente deletado sera enviada(o) para a lixeira e deletado permanentemente depois de 30 dias')
@@ -114,9 +116,9 @@ describe('Clients Page', () => {
         cy.contains(edited_client_data.about)
 
         // Do the same thing, but click 'Yes' at the end to delete the client, and assert the cient is not there anymore
-        cy.get('delete').click()
+        cy.get('@delete').click()
         getByLabel('Sim, deletar cliente').click()
-        cy.get('addedClient').should('not.exist')
+        cy.get('@addedClient').should('not.exist')
     })
   })
 
@@ -132,11 +134,11 @@ describe('Clients Page', () => {
       cy.get('#search-load').should('be.visible')
       cy.get('#results').within(() => { // NOTE - Maybe the #results and #results-list could be the same
         cy.get('ol#results-list').as('list').should('have.length', 10) // NOTE -- This length will be configurable in the future
-        cy.get('list').eq(0).get('.client-card').contains(client)
+        cy.get('@list').eq(0).get('.client-card').contains(client)
       })
       cy.get('#search-load').should('not.be.visible')
 
-      cy.get('search').clear()
+      cy.get('@search').clear()
       cy.get('#search-load').should('be.visible')
       cy.get('#results').should('not.be.visible')
     })
