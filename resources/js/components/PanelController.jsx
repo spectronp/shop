fd260a5eddd34bf96435579b19eca31b0df3dcc4
SpecTro <natplayspectro@gmail.com>
2022-07-28 13:15:35 -0300

    Make PanelController pass reducer to AddFormPanel

--- resources/js/components/PanelController.jsx
+++ resources/js/components/PanelController.jsx
@@ -1,11 +1,11 @@
 import Panel from "../assets/Panel"
 import AddFormPanel from "./AddFormPanel"
 
-export default function PanelController({ activePanel }) {
+export default function PanelController({ activePanel, registerRelevantClient }) {
 
     switch(activePanel) {
         case Panel.ADD_FORM:
-            return <AddFormPanel />
+            return <AddFormPanel registerRelevantClient={registerRelevantClient} />
         default:
             return null
     }
