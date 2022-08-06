f6c36bfd98b1a78deb44de2443e559ec664848aa
SpecTro <natplayspectro@gmail.com>
2022-08-06 09:41:21 -0300

    test(RelevancyReducer): Remove only()

--- tests/jest/RelevancyReducer.test.js
+++ tests/jest/RelevancyReducer.test.js
@@ -10,7 +10,7 @@ test('put client at the start and return the result if lenght < 5', () => {
     expect(result).toEqual(expected)
 })
 
-test.only('put client at the start and remove the last one if lenght > 5', () => {
+test('put client at the start and remove the last one if lenght > 5', () => {
     let initialArray = [1, 2, 3, 4, 5]
     let toBeInserted = 6
     let expected = [6, 1, 2, 3, 4]
