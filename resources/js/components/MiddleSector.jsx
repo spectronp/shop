eb8ff7ca63537a8101c4764e3978cf1e0c298f14
SpecTro <natplayspectro@gmail.com>
2022-08-15 14:05:37 -0300

    (MiddleSector): Add object destructuring to props

--- resources/js/components/MiddleSector.jsx
+++ resources/js/components/MiddleSector.jsx
@@ -3,14 +3,14 @@ import PanelReducer from "../reducers/PanelReducer";
 import ControlTab from "./ControlTab";
 import PanelController from "./PanelController"
 
-export default function MiddleSector(props){
+export default function MiddleSector({ registerRelevantClient }){
 
     const [activePanel, setActivePanel] = useReducer(PanelReducer, null)
 
     return (
         <>
         <ControlTab setActivePanel={setActivePanel} />
-        <PanelController activePanel={activePanel} registerRelevantClient={props.registerRelevantClient } />
+        <PanelController activePanel={activePanel} registerRelevantClient={registerRelevantClient } />
         </>
     )
 }
