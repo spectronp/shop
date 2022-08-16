c9ce50e4aedb82d0fa645ec121bc6ef3e764ed1a
SpecTro <natplayspectro@gmail.com>
2022-08-16 12:25:40 -0300

    (ClientsPage): Add ClientsEditModal
    * Create new state for the modal
    
    * Render modal if editModal.isOpen is true
    
    * Pass setEditModal to CardDisplay

--- resources/js/components/ClientsPage.jsx
+++ resources/js/components/ClientsPage.jsx
@@ -1,15 +1,26 @@
-import { useReducer } from "react"
+import { useReducer, useState } from "react"
 import MiddleSector from "./MiddleSector"
 import CardDisplay from "./CardDisplay"
+import ClientEditModal from "./ClientEditModal"
 import RelevancyReducer from "../reducers/RelevancyReducer"
 
 export default function ClientsPage(){
     const [relevantClients, registerRelevantClient] = useReducer(RelevancyReducer, [])
+    const [editModal, setEditModal] = useState({ isOpen: false })
 
     return (
         <>
+        {
+            editModal.isOpen
+                ? <ClientEditModal client={editModal.client} />
+                : null
+        }
         <MiddleSector registerRelevantClient={registerRelevantClient} />
-        <CardDisplay relevantClients={relevantClients} registerRelevantClient={registerRelevantClient} />
+        <CardDisplay
+            relevantClients={relevantClients}
+            registerRelevantClient={registerRelevantClient}
+            setEditModal={setEditModal}
+        />
         </>
     )
 }
