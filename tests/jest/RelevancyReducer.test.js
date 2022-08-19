6179509346e748de06ba2556eda9bc3182d3b1d6
SpecTro <natplayspectro@gmail.com>
2022-08-19 10:12:55 -0300

    fix(RelevancyReducer): Fix relevant client duplication
    The reducer must not have 2 or more clients with the same id

--- tests/jest/RelevancyReducer.test.js
+++ tests/jest/RelevancyReducer.test.js
@@ -1,19 +1,29 @@
 import RelevancyReducer from '../../resources/js/reducers/RelevancyReducer'
 
 test('put client at the start and return the result if lenght < 5', () => {
-    let initialArray = [1, 2, 3]
-    let toBeInserted = 4
-    let expected = [4, 1, 2, 3]
+    let initial =  [ {id: 1}, {id: 2}, {id: 3} ] //[1, 2, 3]
+    let toBeInserted =  { id: 4 } // 4
+    let expected =   [ {id: 4}, {id: 1}, {id: 2}, {id: 3} ] //[4, 1, 2, 3]
+
+    let result = RelevancyReducer(initial, toBeInserted)
+
+    expect(result).toEqual(expected)
+})
+
+test('put client at the start and remove the last one if lenght > 5', () => {
+    let initialArray = [ {id: 1}, {id: 2}, {id: 3}, {id: 4}, {id: 5} ]
+    let toBeInserted = {id: 6}
+    let expected = [ {id: 6}, {id: 1}, {id: 2}, {id: 3}, {id: 4} ]
 
     let result = RelevancyReducer(initialArray, toBeInserted)
 
     expect(result).toEqual(expected)
 })
 
-test('put client at the start and remove the last one if lenght > 5', () => {
-    let initialArray = [1, 2, 3, 4, 5]
-    let toBeInserted = 6
-    let expected = [6, 1, 2, 3, 4]
+test('remove client with same id', () => {
+    let initialArray = [ {id: 1}, {id: 2}, {id: 3}, {id: 4} ]
+    let toBeInserted = {id: 3}
+    let expected = [ {id: 3}, {id: 1}, {id: 2}, {id: 4} ]
 
     let result = RelevancyReducer(initialArray, toBeInserted)
 
