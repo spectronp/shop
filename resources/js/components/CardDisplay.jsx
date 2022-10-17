c1f1d9542d5396ac99a1938acfa1e2fad76b7aee
SpecTro <natplayspectro@gmail.com>
2022-10-17 15:26:26 -0300

    Render SearchResult component and pass searchInput to it

--- resources/js/components/CardDisplay.jsx
+++ resources/js/components/CardDisplay.jsx
@@ -1,8 +1,12 @@
 import MostRelevant from './MostRelevant'
+import SearchResult from './SearchResult'
 
-export default function CardDisplay({ relevantClients, setEditModal }){
-    return <MostRelevant
+export default function CardDisplay({ searchInput, relevantClients, setEditModal }){
+    return <>
+    <SearchResult searchInput={searchInput} />
+    <MostRelevant
         relevantClients={relevantClients}
         setEditModal={setEditModal}
     />
+    </>
 }
