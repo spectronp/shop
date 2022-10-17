37e644d4ba49bd7687d03647296cce68d1f71729
SpecTro <natplayspectro@gmail.com>
2022-10-17 15:20:46 -0300

    Add searchInput state and make page render Search component

--- resources/js/components/ClientsPage.jsx
+++ resources/js/components/ClientsPage.jsx
@@ -1,15 +1,18 @@
 import { useReducer, useState } from "react"
+import Search from "./Search"
 import MiddleSector from "./MiddleSector"
 import CardDisplay from "./CardDisplay"
 import ClientEditModal from "./ClientEditModal"
 import RelevancyReducer from "../reducers/RelevancyReducer"
 
 export default function ClientsPage(){
+    const [searchInput, setSearchInput] = useState()
     const [relevantClients, registerRelevantClient] = useReducer(RelevancyReducer, [])
     const [editModal, setEditModal] = useState({ isOpen: false })
 
     return (
         <>
+        <Search setSearchInput={setSearchInput} />
         {
             editModal.isOpen
                 ? <ClientEditModal
@@ -21,6 +24,7 @@ export default function ClientsPage(){
         }
         <MiddleSector registerRelevantClient={registerRelevantClient} />
         <CardDisplay
+            searchInput={searchInput}
             relevantClients={relevantClients}
             registerRelevantClient={registerRelevantClient}
             setEditModal={setEditModal}
